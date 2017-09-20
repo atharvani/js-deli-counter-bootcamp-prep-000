767ec981e24d83f193a279500c73fe68c970e405
Shital Sanap <shital.sanap@gmail.com>
2017-09-20 02:27:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function takeANumber(katzDeli, name)
 {
   var retString
     katzDeli.push(name)
-    retString = "welcome " + name + "! You are number " + katzDeli.length + " in line."
+    retString = "Welcome " + name + ". You are number " + katzDeli.length + " in line."
     return retString;
 }
