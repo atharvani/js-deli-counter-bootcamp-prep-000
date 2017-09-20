4b99d5077aeb3109a476aa37dbfe4b45d80d8ca5
Shital Sanap <shital.sanap@gmail.com>
2017-09-20 02:44:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,20 @@ function takeANumber(katzDeli, name)
     retString = "Welcome, " + name + ". You are number " + katzDeli.length + " in line."
     return retString;
 }
+
+function nowServing(katzDeliLine)
+{
+  var retString
+  if(katzDeliLine.length === 0)
+  {
+    retString = "There is nobody waiting to be served!"
+    
+  }
+  else 
+  {
+    var number = katzDeliLine.length
+      retString = "Currently serving " + katzDeliLine[0];
+      katzDeliLine.shift();
+    }
+    return retString
+}
